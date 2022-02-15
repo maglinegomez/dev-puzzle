45552655152a638a868e2af5cfa2ed9d0be11ee6
MEugine1 <Magline.Eugine1@t-mobile.com>
2022-02-15 21:30:09 +0530

    Committing Undo add and remove from reading list

--- apps/okreads-e2e/src/integration/reading-list.spec.ts
+++ apps/okreads-e2e/src/integration/reading-list.spec.ts
@@ -11,4 +11,24 @@ describe('When: I use the reading list feature', () => {
       'My Reading List'
     );
   });
+
+  it('Then: I should be able to undo my added book', async () => {
+    cy.get('input[type="search"]').type('javascript');
+    cy.get('form').submit();
+    cy.get('[data-testing="add-to-reading-list"]:enabled').first().should('exist').click();
+    cy.get('.mat-simple-snackbar-action button').click();
+    cy.get('[data-testing="toggle-reading-list"]').click();
+    cy.get('[data-testing="remove-reading-list"]').should('have.length', 0);
+  });
+
+  it('Then: I should be able to undo my removed book', async () => {
+    cy.get('input[type="search"]').type('javascript');
+    cy.get('form').submit();
+    cy.get('[data-testing="add-to-reading-list"]:enabled').first().should('exist').click();
+    cy.get('[data-testing="toggle-reading-list"]').click();
+    cy.get('[data-testing="remove-reading-list"]').should('have.length', 1);
+    cy.get('[data-testing="remove-reading-list"]').click();
+    cy.get('.mat-simple-snackbar-action button').click();
+    cy.get('[data-testing="remove-reading-list"]').should('have.length', 1);
+  });
 });
--- libs/books/data-access/src/lib/+state/reading-list.effects.ts
+++ libs/books/data-access/src/lib/+state/reading-list.effects.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Actions, createEffect, ofType, OnInitEffects } from '@ngrx/effects';
-import { of } from 'rxjs';
-import { catchError, concatMap, exhaustMap, map } from 'rxjs/operators';
+import { of, EMPTY } from 'rxjs';
+import { catchError, concatMap, exhaustMap, map, mergeMap } from 'rxjs/operators';
 import { ReadingListItem } from '@tmo/shared/models';
 import * as ReadingListActions from './reading-list.actions';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable()
 export class ReadingListEffects implements OnInitEffects {
@@ -27,24 +28,44 @@ export class ReadingListEffects implements OnInitEffects {
   addBook$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ReadingListActions.addToReadingList),
-      concatMap(({ book }) =>
+      concatMap(({ book, skipUndo }) =>
         this.http.post('/api/reading-list', book).pipe(
-          map(() => ReadingListActions.confirmedAddToReadingList({ book })),
+          mergeMap(() => {
+            const item = { ...book, bookId: book.id };
+            if (!skipUndo) {
+              return of(ReadingListActions.undoAction({
+                message: "Book is added to reading list",
+                undoAction: ReadingListActions.removeFromReadingList({ item, skipUndo: true })
+              }));
+            }
+            return EMPTY;
+          }),
           catchError(() =>
             of(ReadingListActions.failedAddToReadingList({ book }))
           )
         )
-      )
+      ),
+
     )
   );
 
   removeBook$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ReadingListActions.removeFromReadingList),
-      concatMap(({ item }) =>
+      concatMap(({ item, skipUndo }) =>
         this.http.delete(`/api/reading-list/${item.bookId}`).pipe(
-          map(() =>
-            ReadingListActions.confirmedRemoveFromReadingList({ item })
+          mergeMap(() => {
+            const book = { ...item, id: item.bookId };
+            if (!skipUndo) {
+              return of(ReadingListActions.undoAction({
+                message: "Book is removed from reading list",
+                undoAction: ReadingListActions.addToReadingList({ book, skipUndo: true })
+              })
+              );
+            }
+            return EMPTY;
+          }
+
           ),
           catchError(() =>
             of(ReadingListActions.failedRemoveFromReadingList({ item }))
@@ -54,9 +75,25 @@ export class ReadingListEffects implements OnInitEffects {
     )
   );
 
+  undoEffect$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ReadingListActions.undoAction),
+      concatMap(({ message, undoAction }) => this.snackBar.open(message, "Undo", { duration: 5000 }).onAction()
+        .pipe(
+          map(() => undoAction)
+        )
+      ),
+    )
+  );
+
+
   ngrxOnInitEffects() {
     return ReadingListActions.init();
   }
 
-  constructor(private actions$: Actions, private http: HttpClient) {}
+  constructor(
+    private actions$: Actions,
+    private http: HttpClient,
+    private snackBar: MatSnackBar
+  ) { }
 }
